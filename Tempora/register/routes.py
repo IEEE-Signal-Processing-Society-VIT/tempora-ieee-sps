c1a4604e46bbdad1291b6a80ebf79b080700565e
akhilsrinivasp <71825776+akhilsrinivasp@users.noreply.github.com>
2022-07-03 05:48:22 +0530

    Delete Tempora directory

--- Tempora/register/routes.py
+++
@@ -1,4 +0,0 @@
-from atexit import register
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
-
-register_blueprint=Blueprint('register', __name__, url_prefix='/register')
